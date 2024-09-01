0fdbf6aa779040678a5e79676a964dd626a3b2fe
Daniel Yan <danielyanzhengyuhan@gmail.com>
2024-09-01 03:37:24 -0500

    added analysis

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }

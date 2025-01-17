002dbb31ca5872c065148e409830933889470168
Daniel Yan <danielyanzhengyuhan@gmail.com>
2025-01-16 22:45:35 -0600

    add wechat group link

---
+++ components/Chat.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ChatBubbleIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-message-circle"
+    >
+      <path d="M21 11.5a8.38 8.38 0 0 1-1.2 4.4 8.5 8.5 0 0 1-7.8 4.6 8.38 8.38 0 0 1-4.4-1.2L2 22l2.7-5a8.38 8.38 0 0 1-1.2-4.4 8.5 8.5 0 0 1 4.6-7.8 8.38 8.38 0 0 1 4.4-1.2 8.5 8.5 0 0 1 8.5 8.5z" />
+    </svg>
+  );
+};
+
+export default ChatBubbleIcon;
